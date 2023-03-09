87ea0c91e0a43319f598d0a0ec76af1a33910e34
meghanamuthukumar <meghanamuthukumar14@gmail.com>
2023-03-08 22:45:43 -0500

    initial commit

---
+++ backend/src/main/java/com/jason/movingInc/MovingIncApplication.java
@@ -0,0 +1,13 @@
+package com.jason.movingInc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovingIncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovingIncApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/jason/movingInc/MovingIncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jason.movingInc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovingIncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
